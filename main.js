5e34d6bfa79e770de537e5d4b6162f6afbcfc53a
Milo Wissig <mwissig@gmail.com>
2021-04-30 17:05:49 -0400

    media queries

--- main.js
+++ main.js
@@ -6,28 +6,26 @@ var art = [
           medium: "Oil on Panel",
           description: "description",
           filename: "florida.jpg",
-          thumbnail: "florida.jpg"},
-
+        },
           {title: "title",
           year: 2014,
           medium: "Oil on Panel",
           description: "description",
           filename: "haircut.jpg",
-          thumbnail: "haircut.jpg"},
-
+        },
           {title: "title",
           year: 2014,
           medium: "Oil on Panel",
           description: "description",
           filename: "hottub.jpg",
-          thumbnail: "hottub.jpg"},
+        },
 
           {title: "title",
           year: 2015,
           medium: "Oil on Panel",
           description: "description",
           filename: "staples.jpg",
-          thumbnail: "staples.jpg"}
+                  }
           ];
 
 var lightbox_container = document.getElementById("lightbox_container");
@@ -36,10 +34,10 @@ function printLightbox () {
 var i;
 for (i = 0; i < art.length; i++) {
   var addArt = document.createElement("div");
-  addArt.setAttribute("id", "thumbnail"+i);
+  addArt.setAttribute("id", "filename"+i);
   addArt.setAttribute("class", "lightbox_entry");
   addArt.style.backgroundImage = `url("images/${art[i].filename}")`;
-  addArt.innerHTML = `<a href="images/${art[i].filename}" data-lightbox="art" data-title="${art[i].title}" data-description="${art[i].description}" data-year="${art[i].year}" data-medium="${art[i].medium}"><img src="images/${art[i].thumbnail}" class="thumbnail"></a>`;
+  addArt.innerHTML = `<a href="images/${art[i].filename}" data-lightbox="art" data-title="${art[i].title}" data-description="${art[i].description}" data-year="${art[i].year}" data-medium="${art[i].medium}"><img src="images/${art[i].filename}" class="thumbnail"></a>`;
   lightbox_container.appendChild(addArt);
 }
 }
