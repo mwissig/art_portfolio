73248bbca182eff97fa927f5295d1f0c1d627e06
Milo Wissig <mwissig@gmail.com>
2021-04-30 15:45:17 -0400

    header bar

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
 {console.log("hi");}
+
+var art = [{title: "title",
+          year: 2010,
+          description: "description",
+          filename: "florida.jpg",
+          thumbnail: "florida.jpg"},
+          {title: "title",
+          year: 2010,
+          description: "description",
+          filename: "haircut.jpg",
+          thumbnail: "haircut.jpg"},
+          {title: "title",
+          year: 2010,
+          description: "description",
+          filename: "hottub.jpg",
+          thumbnail: "hottub.jpg"},
+          {title: "title",
+          year: 2010,
+          description: "description",
+          filename: "staples.jpg",
+          thumbnail: "staples.jpg"}];
+
+var lightbox_container = document.getElementById("lightbox_container");
+
+function printLightbox () {
+var i;
+for (i = 0; i < art.length; i++) {
+  var addArt = document.createElement("div");
+  addArt.setAttribute("id", "thumbnail"+i);
+  addArt.setAttribute("class", "lightbox_entry");
+  addArt.style.backgroundImage = `url("images/${art[i].filename}")`;
+  addArt.innerHTML = `<a href="images/${art[i].filename}" data-lightbox="art" data-title="${art[i].title}"><img src="images/${art[i].thumbnail}" class="thumbnail"></a>`;
+  lightbox_container.appendChild(addArt);
+}
+}
+
+printLightbox();
