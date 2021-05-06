a5bc77f6d60a164cc580ad0273a67c2377060b1e
Milo Wissig <mwissig@gmail.com>
2021-05-06 19:27:01 -0400

    update

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
   console.log("hi");
 }
 
-var art = [
-  {
-    title: "title",
+var art = [{
+    title: "Apple Pie",
     year: 2016,
     medium: "Oil on Panel",
+    size: "16in x 12in",
     description: "description",
     filename: "pie",
     category: "Paintings 2014-present"
@@ -15,22 +15,34 @@ var art = [
     title: "title",
     year: 2015,
     medium: "Oil on Panel",
+    size: "",
     description: "description",
     filename: "staples",
     category: "Paintings 2014-present"
   },
   {
-    title: "title",
+    title: "Hot Tub",
     year: 2014,
     medium: "Oil on Panel",
+    size: "9in x 12in",
     description: "description",
     filename: "hottub",
     category: "Paintings 2014-present"
   },
   {
-    title: "title",
+    title: "Loft",
+    year: 2014,
+    medium: "Oil on Panel",
+    size: "6in x 8in",
+    description: "description",
+    filename: "loft",
+    category: "Paintings 2014-present"
+  },
+  {
+    title: "New York Hoist",
     year: 2015,
     medium: "Oil on Panel",
+    size: "",
     description: "description",
     filename: "hoist",
     category: "Paintings 2014-present"
@@ -39,6 +51,7 @@ var art = [
     title: "title",
     year: 2010,
     medium: "Oil on Panel",
+    size: "9ft x 6ft",
     description: "description",
     filename: "florida",
     category: "Paintings 2010-2014"
@@ -47,6 +60,7 @@ var art = [
     title: "title",
     year: 2013,
     medium: "Oil on Panel",
+    size: "",
     description: "description",
     filename: "018",
     category: "Paintings 2010-2014"
@@ -55,6 +69,7 @@ var art = [
     title: "title",
     year: 2013,
     medium: "Oil on Panel",
+    size: "",
     description: "description",
     filename: "019",
     category: "Paintings 2010-2014"
@@ -63,6 +78,7 @@ var art = [
     title: "title",
     year: 2013,
     medium: "Oil on Panel",
+    size: "",
     description: "description",
     filename: "020",
     category: "Paintings 2010-2014"
@@ -71,6 +87,7 @@ var art = [
     title: "title",
     year: 2013,
     medium: "Oil on Panel",
+    size: "",
     description: "description",
     filename: "021",
     category: "Paintings 2010-2014"
@@ -79,97 +96,172 @@ var art = [
     title: "title",
     year: 2013,
     medium: "Oil on Panel",
+    size: "",
     description: "description",
     filename: "022",
     category: "Paintings 2010-2014"
   },
   {
     title: "title",
-    year: 2014,
+    year: 2013,
     medium: "Oil on Panel",
+    size: "",
+    description: "description",
+    filename: "024",
+    category: "Paintings 2010-2014"
+  },
+  {
+    title: "title",
+    year: 2013,
+    medium: "Oil on Panel",
+    size: "",
+    description: "description",
+    filename: "025",
+    category: "Paintings 2010-2014"
+  },
+  {
+    title: "title",
+    year: 2013,
+    medium: "Oil on Panel",
+    size: "",
+    description: "description",
+    filename: "026",
+    category: "Paintings 2010-2014"
+  },
+  {
+    title: "title",
+    year: 2013,
+    medium: "Oil on Panel",
+    size: "",
+    description: "description",
+    filename: "027",
+    category: "Paintings 2010-2014"
+  },
+  {
+    title: "Haircut",
+    year: 2013,
+    medium: "Oil on Panel",
+    size: "9in x 12in",
     description: "description",
     filename: "haircut",
     category: "Paintings 2010-2014"
   },
   {
-    title: "Metro Drawings",
+    title: "Shots & Shade Drawings: Alex",
     year: "2019-2020",
     medium: "Conte and Pencil on Dyed Paper",
-    description: "description",
+    size: "13in x 16.5in",
+    description: "A series of drawings done during 'Shots & Shade: A Queer Drink & Draw' hosted by <a href='https://www.instagram.com/brieyob'>@brieyob</a> at <a href='https://www.metropolitanbarny.com/'>Metropolitan Bar</a> in Brooklyn, NY.",
     filename: "metro01",
-    category: "Metropolitan Bar Drawings"
+    category: "Shots & Shade Drawings"
   },
   {
-    title: "Metro Drawings",
+    title: "Shots & Shade Drawings: Alex",
     year: "2019-2020",
     medium: "Conte and Pencil on Dyed Paper",
-    description: "description",
+    size: "13in x 16.5in",
+    description: "A series of drawings done during 'Shots & Shade: A Queer Drink & Draw' hosted by <a href='https://www.instagram.com/brieyob'>@brieyob</a> at <a href='https://www.metropolitanbarny.com/'>Metropolitan Bar</a> in Brooklyn, NY.",
     filename: "metro02",
-    category: "Metropolitan Bar Drawings"
+    category: "Shots & Shade Drawings"
   },
   {
-    title: "Metro Drawings",
+    title: "Shots & Shade Drawings: Noah",
     year: "2019-2020",
     medium: "Conte and Pencil on Dyed Paper",
-    description: "description",
+    size: "13in x 16.5in",
+    description: "A series of drawings done during 'Shots & Shade: A Queer Drink & Draw' hosted by <a href='https://www.instagram.com/brieyob'>@brieyob</a> at <a href='https://www.metropolitanbarny.com/'>Metropolitan Bar</a> in Brooklyn, NY.",
     filename: "metro03",
-    category: "Metropolitan Bar Drawings"
+    category: "Shots & Shade Drawings"
   },
   {
-    title: "Metro Drawings",
+    title: "Shots & Shade Drawings: Kirsten Leigh",
     year: "2019-2020",
     medium: "Conte and Pencil on Dyed Paper",
-    description: "description",
+    size: "13in x 16.5in",
+    description: "A series of drawings done during 'Shots & Shade: A Queer Drink & Draw' hosted by <a href='https://www.instagram.com/brieyob'>@brieyob</a> at <a href='https://www.metropolitanbarny.com/'>Metropolitan Bar</a> in Brooklyn, NY. <a href='https://www.instagram.com/kleoleee/'>@kleoleee</a>",
     filename: "metro04",
-    category: "Metropolitan Bar Drawings"
+    category: "Shots & Shade Drawings"
   },
   {
-    title: "Metro Drawings",
+    title: "Shots & Shade Drawings: Kirsten Leigh",
     year: "2019-2020",
     medium: "Conte and Pencil on Dyed Paper",
-    description: "description",
+    size: "13in x 16.5in",
+    description: "A series of drawings done during 'Shots & Shade: A Queer Drink & Draw' hosted by <a href='https://www.instagram.com/brieyob'>@brieyob</a> at <a href='https://www.metropolitanbarny.com/'>Metropolitan Bar</a> in Brooklyn, NY. <a href='https://www.instagram.com/kleoleee/'>@kleoleee</a>",
     filename: "metro05",
-    category: "Metropolitan Bar Drawings"
+    category: "Shots & Shade Drawings"
   },
   {
-    title: "Metro Drawings",
+    title: "Shots & Shade Drawings: Kirsten Leigh",
     year: "2019-2020",
     medium: "Conte and Pencil on Dyed Paper",
-    description: "description",
+    size: "13in x 16.5in",
+    description: "A series of drawings done during 'Shots & Shade: A Queer Drink & Draw' hosted by <a href='https://www.instagram.com/brieyob'>@brieyob</a> at <a href='https://www.metropolitanbarny.com/'>Metropolitan Bar</a> in Brooklyn, NY. <a href='https://www.instagram.com/kleoleee/'>@kleoleee</a>",
     filename: "metro06",
-    category: "Metropolitan Bar Drawings"
+    category: "Shots & Shade Drawings"
   },
   {
-    title: "Metro Drawings",
+    title: "Shots & Shade Drawings: Nogga",
     year: "2019-2020",
     medium: "Conte and Pencil on Dyed Paper",
-    description: "description",
+    size: "13in x 16.5in",
+    description: "A series of drawings done during 'Shots & Shade: A Queer Drink & Draw' hosted by <a href='https://www.instagram.com/brieyob'>@brieyob</a> at <a href='https://www.metropolitanbarny.com/'>Metropolitan Bar</a> in Brooklyn, NY.",
     filename: "metro07",
-    category: "Metropolitan Bar Drawings"
+    category: "Shots & Shade Drawings"
   },
   {
-    title: "Metro Drawings",
-    year: "2019-2020",
+    title: "Shots & Shade Drawings: Wanda Whatever",
+    year: 2020,
     medium: "Conte and Pencil on Dyed Paper",
-    description: "description",
+    size: "13in x 16.5in",
+    description: "A series of drawings done during 'Shots & Shade: A Queer Drink & Draw' hosted by <a href='https://www.instagram.com/brieyob'>@brieyob</a> at <a href='https://www.metropolitanbarny.com/'><a href='https://www.metropolitanbarny.com/'>Metropolitan Bar</a></a> in Brooklyn, NY. <a href='https://www.instagram.com/wanda.whatever/'>@wanda.whatever</a>",
     filename: "metro08",
-    category: "Metropolitan Bar Drawings"
+    category: "Shots & Shade Drawings"
   },
   {
-    title: "Metro Drawings",
-    year: "2019-2020",
+    title: "Shots & Shade Drawings: Wanda Whatever",
+    year: 2020,
     medium: "Conte and Pencil on Dyed Paper",
-    description: "description",
+    size: "13in x 16.5in",
+    description: "A series of drawings done during 'Shots & Shade: A Queer Drink & Draw' hosted by <a href='https://www.instagram.com/brieyob'>@brieyob</a> at <a href='https://www.metropolitanbarny.com/'>Metropolitan Bar</a> in Brooklyn, NY. <a href='https://www.instagram.com/wanda.whatever/'>@wanda.whatever</a>",
     filename: "metro09",
-    category: "Metropolitan Bar Drawings"
+    category: "Shots & Shade Drawings"
   },
   {
-    title: "Metro Drawings",
-    year: "2019-2020",
+    title: "Shots & Shade Drawings: Wanda Whatever",
+    year: 2020,
     medium: "Conte and Pencil on Dyed Paper",
-    description: "description",
+    size: "13in x 16.5in",
+    description: "A series of drawings done during 'Shots & Shade: A Queer Drink & Draw' hosted by <a href='https://www.instagram.com/brieyob'>@brieyob</a> at <a href='https://www.metropolitanbarny.com/'>Metropolitan Bar</a> in Brooklyn, NY. <a href='https://www.instagram.com/wanda.whatever/'>@wanda.whatever</a>",
     filename: "metro10",
-    category: "Metropolitan Bar Drawings"
+    category: "Shots & Shade Drawings"
+  },
+  {
+    title: "Commission",
+    year: 2017,
+    medium: "Oil on Panel",
+    size: "7in x 5in",
+    description: "A commissioned portrait",
+    filename: "portrait01",
+    category: "Commissions and Portraits"
+  },
+  {
+    title: "Commission",
+    year: 2017,
+    medium: "Oil on Panel",
+    size: "6in x 4in",
+    description: "A commissioned portrait",
+    filename: "portrait02",
+    category: "Commissions and Portraits"
+  },
+  {
+    title: "Commission",
+    year: 2017,
+    medium: "Ink and gouache on board",
+    size: "8in x 8in",
+    description: "A commissioned dog portrait",
+    filename: "dog01",
+    category: "Commissions and Portraits"
   }
 ];
 var titles = [];
@@ -178,15 +270,14 @@ var lightbox_container = document.getElementById("lightbox_container");
 function printLightbox() {
   var i;
   for (i = 0; i < art.length; i++) {
-  var strippedTitle =  art[i].category.replace(/\s/g,'')
+    var strippedTitle = art[i].category.replace(/\s/g, '')
     if (i <= 0) {
       var addTitle = document.createElement("h3");
       addTitle.setAttribute("id", strippedTitle);
       addTitle.innerHTML = art[i].category;
       lightbox_container.appendChild(addTitle);
       titles.push(art[i].category);
-    }
-    else if (art[i].category != art[i - 1].category) {
+    } else if (art[i].category != art[i - 1].category) {
       var addTitle = document.createElement("h3");
       addTitle.setAttribute("id", strippedTitle);
       addTitle.innerHTML = art[i].category;
@@ -194,21 +285,21 @@ function printLightbox() {
       titles.push(art[i].category);
     };
 
-  var addArt = document.createElement("div");
-  addArt.setAttribute("id", "filename" + i);
-  addArt.setAttribute("class", "lightbox_entry");
-  addArt.style.backgroundImage = `url("images/${art[i].filename}.jpg")`;
-  addArt.innerHTML = `<a href="images/${art[i].filename}.jpg" data-lightbox="${art[i].category}" data-title="${art[i].title}" data-description="${art[i].description}" data-year="${art[i].year}" data-medium="${art[i].medium}"><img src="thumbnails/${art[i].filename}.jpg" class="thumbnail"></a>`;
-  lightbox_container.appendChild(addArt);
+    var addArt = document.createElement("div");
+    addArt.setAttribute("id", "filename" + i);
+    addArt.setAttribute("class", "lightbox_entry");
+    addArt.style.backgroundImage = `url("thumbnails/${art[i].filename}.jpg")`;
+    addArt.innerHTML = `<a href="images/${art[i].filename}.jpg" data-lightbox="${art[i].category}" data-title="${art[i].title}" data-description="${art[i].description}" data-year="${art[i].year}" data-medium="${art[i].medium}" data-size="${art[i].size}"><img src="thumbnails/${art[i].filename}.jpg" class="thumbnail"></a>`;
+    lightbox_container.appendChild(addArt);
 
-}
-var j;
-for (j = 0; j < titles.length; j++) {
-var titlesMenu = document.getElementById("titles_menu");
-var addMenuItem = document.createElement("li");
-addMenuItem.innerHTML = `<a href="#${titles[j].replace(/\s/g,'')}">${titles[j]}</a>`
-titlesMenu.appendChild(addMenuItem);
-}
+  }
+  var j;
+  for (j = 0; j < titles.length; j++) {
+    var titlesMenu = document.getElementById("titles_menu");
+    var addMenuItem = document.createElement("li");
+    addMenuItem.innerHTML = `<a href="#${titles[j].replace(/\s/g,'')}">${titles[j]}</a>`
+    titlesMenu.appendChild(addMenuItem);
+  }
 };
 
 
